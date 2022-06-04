6ec46fa860e63665ecde7d9739dc7a7fb76ae536
Eugene Kudelya <kudel89@gmail.com>
2022-06-05 01:27:40 +0300

    The bottom section of the GameBuy cart has alignmented

--- src/components/game-buy/GameBuy.js
+++ src/components/game-buy/GameBuy.js
@@ -4,7 +4,7 @@ import styles from "./GameBuy.module.css";
 
 export const GameBuy = ({ game }) => {
   return (
-    <div className={styles.button}>
+    <div className={styles.container}>
       <span className={styles.price}>{game.price} грн.</span>
       <Button type="primary" onClick={() => null}>
         В корзину
