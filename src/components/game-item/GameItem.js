d896894d9edaa459c3577d4e00475f35ba954311
Eugene Kudelya <kudel89@gmail.com>
2022-06-04 16:00:35 +0300

    Some changes for the GameItem component

--- src/components/game-item/GameItem.js
+++ src/components/game-item/GameItem.js
@@ -1,14 +1,20 @@
 import React from "react";
+import { GameCover } from "../game-cover/GameCover";
+import { GameBuy } from "../game-buy/GameBuy";
 import styles from "./GameItem.module.css";
 
 export const GameItem = ({ game }) => {
   return (
     <div className={styles.card}>
+      <GameCover image={game.image} />
+
       <div className={styles.detailes}>
         <span className={styles.title}>{game.title}</span>
         <div className={styles.genre}>{game.genres.map((genre) => genre)}</div>
         <p className={styles.description}>{game.description}</p>
-        <button className={styles.buy}>Buy</button>
+        <div className={styles.buy}>
+          <GameBuy />
+        </div>
       </div>
     </div>
   );
