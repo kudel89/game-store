926306b1ead2b9093e90686db0ce45675cf9cc48
Eugene Kudelya <kudel89@gmail.com>
2022-06-06 14:03:31 +0300

    Added key property for the GameGenre

--- src/components/game-item/GameItem.js
+++ src/components/game-item/GameItem.js
@@ -15,7 +15,7 @@ export const GameItem = ({ game }) => {
             <span className={styles.title}>{game.title}</span>
             <div className={styles.genre}>
               {game.genres.map((genre) => (
-                <GameGenre genre={genre} />
+                <GameGenre genre={genre} key={genre} />
               ))}
             </div>
             <p className={styles.description}>{game.description}</p>
