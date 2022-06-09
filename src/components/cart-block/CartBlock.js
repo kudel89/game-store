aba65a9ded23e3c10d684e8983b56648d376e274
Eugene Kudelya <kudel89@gmail.com>
2022-06-09 19:28:55 +0300

    Added totalPrice function to the CartBlock

--- src/components/cart-block/CartBlock.js
+++ src/components/cart-block/CartBlock.js
@@ -1,12 +1,18 @@
 import React from "react";
 import { BiCartAlt } from "react-icons/bi";
+import { useSelector } from "react-redux";
+import { CartMenu } from "../cart-menu";
+import { calcTotalPrice } from "../../utils";
 import styles from "./CartBlock.module.css";
 
 export const CartBlock = () => {
+  const items = useSelector((state) => state.cart.itemsInCart);
+  const totalPrice = calcTotalPrice(items);
   return (
     <div className={styles.container}>
       <BiCartAlt size={25} className={styles.icon} />
-      <span className={styles.price}>999 грн.</span>
+      <span className={styles.price}>{totalPrice} грн.</span>
+      <CartMenu items={items} onClick={() => null} />
     </div>
   );
 };
