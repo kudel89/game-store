8b7acdc720f81d1edcaf3610203b2206052ca378
Eugene Kudelya <kudel89@gmail.com>
2022-06-09 18:57:20 +0300

    Created handleClick function in the GameBuy

--- src/components/game-buy/GameBuy.js
+++ src/components/game-buy/GameBuy.js
@@ -1,12 +1,22 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { setItemInCart } from "../../redux/cart/reducer";
 import { Button } from "../button/Button";
 import styles from "./GameBuy.module.css";
 
 export const GameBuy = ({ game }) => {
+  const dispatch = useDispatch();
+
+  const handleClick = (e) => {
+    console.log("test");
+    e.stopPropagation();
+    dispatch(setItemInCart(game));
+  };
+
   return (
     <div className={styles.container}>
       <span className={styles.price}>{game.price} грн.</span>
-      <Button type="primary" onClick={() => null}>
+      <Button type="primary" onClick={handleClick}>
         В корзину
       </Button>
     </div>
