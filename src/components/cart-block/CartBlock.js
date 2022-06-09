ebbbebd8dbbdf71703dddc17e8ff0cc19664314f
Eugene Kudelya <kudel89@gmail.com>
2022-06-09 19:47:46 +0300

    Added logic for visible of the CartMenu and added the ItemsInCart

--- src/components/cart-block/CartBlock.js
+++ src/components/cart-block/CartBlock.js
@@ -1,18 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import { BiCartAlt } from "react-icons/bi";
 import { useSelector } from "react-redux";
 import { CartMenu } from "../cart-menu";
+import { ItemsInCart } from "../items-in-cart/ItemsInCart";
 import { calcTotalPrice } from "../../utils";
 import styles from "./CartBlock.module.css";
 
 export const CartBlock = () => {
+  const [isCartMenuVisible, setIsCartMenuVisible] = useState(false);
   const items = useSelector((state) => state.cart.itemsInCart);
   const totalPrice = calcTotalPrice(items);
+
   return (
     <div className={styles.container}>
-      <BiCartAlt size={25} className={styles.icon} />
-      <span className={styles.price}>{totalPrice} грн.</span>
-      <CartMenu items={items} onClick={() => null} />
+      <ItemsInCart quantity={items.length} />
+      <BiCartAlt
+        size={25}
+        className={styles.icon}
+        onClick={() => setIsCartMenuVisible(!isCartMenuVisible)}
+      />
+      {totalPrice > 0 && (
+        <span className={styles.price}>{totalPrice} грн.</span>
+      )}
+      {isCartMenuVisible && <CartMenu items={items} onClick={() => null} />}
     </div>
   );
 };
