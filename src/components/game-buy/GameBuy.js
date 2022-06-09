5cdbc35a62eee5f2d8526bf716bd1259176b781c
Eugene Kudelya <kudel89@gmail.com>
2022-06-09 19:28:26 +0300

    Added the logic for adding or deleting game in the cart

--- src/components/game-buy/GameBuy.js
+++ src/components/game-buy/GameBuy.js
@@ -1,23 +1,31 @@
 import React from "react";
-import { useDispatch } from "react-redux";
-import { setItemInCart } from "../../redux/cart/reducer";
+import { useDispatch, useSelector } from "react-redux";
+import { deleteItemFromCart, setItemInCart } from "../../redux/cart/reducer";
 import { Button } from "../button/Button";
 import styles from "./GameBuy.module.css";
 
 export const GameBuy = ({ game }) => {
   const dispatch = useDispatch();
+  const items = useSelector((state) => state.cart.itemsInCart);
+  const isItemInCart = items.some((item) => item.id === game.id);
 
   const handleClick = (e) => {
-    console.log("test");
     e.stopPropagation();
-    dispatch(setItemInCart(game));
+    if (isItemInCart) {
+      dispatch(deleteItemFromCart(game.id));
+    } else {
+      dispatch(setItemInCart(game));
+    }
   };
 
   return (
     <div className={styles.container}>
       <span className={styles.price}>{game.price} грн.</span>
-      <Button type="primary" onClick={handleClick}>
-        В корзину
+      <Button
+        type={isItemInCart ? "secondary" : "primary"}
+        onClick={handleClick}
+      >
+        {isItemInCart ? "Убрать из корзины" : "В корзину"}
       </Button>
     </div>
   );
