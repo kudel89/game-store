a6598126567b34b31640852f3bff1ce01a010012
Eugene Kudelya <kudel89@gmail.com>
2022-06-09 19:56:27 +0300

    Added CartItem into the CartMenu component

--- src/components/cart-menu/CartMenu.js
+++ src/components/cart-menu/CartMenu.js
@@ -1,13 +1,23 @@
 import React from "react";
 import { calcTotalPrice } from "../../utils";
 import { Button } from "./../button/Button";
+import { CartItem } from "./../cart-item";
 import styles from "./CartMenu.module.css";
 
 export const CartMenu = ({ items, onClick }) => {
   return (
     <div className={styles.container}>
       <div className={[styles["game-list"]]}>
-        {items.length > 0 ? items.map((game) => game.title) : "Корзина пуста"}
+        {items.length > 0
+          ? items.map((game) => (
+              <CartItem
+                key={game.title}
+                price={game.price}
+                title={game.title}
+                id={game.id}
+              />
+            ))
+          : "Корзина пуста"}
       </div>
       {items.length > 0 ? (
         <div className={[styles["game-arrange"]]}>
