10a2f4021fd3f4e42a87f0ab9c0be0a9f6677198
Eugene Kudelya <kudel89@gmail.com>
2022-06-10 11:14:37 +0300

    Created the GamePage component

---
+++ src/pages/game-page/GamePage.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { GameCover } from "../../components/game-cover/GameCover";
+import { GameGenre } from "../../components/game-genre/GameGenre";
+import { GameBuy } from "./../../components/game-buy/GameBuy";
+import styles from "./GamePage.module.css";
+
+export const GamePage = () => {
+  const game = useSelector((state) => state.game.currentGame);
+
+  if (!game) return null;
+
+  return (
+    <div className={styles.container}>
+      <h1>{game.title}</h1>
+      <div className={styles.content}>
+        <div className={styles.video}>
+          <iframe
+            width="90%"
+            height="400px"
+            src={game.video}
+            title="YouTube Video Player"
+            frameBorder="0"
+          ></iframe>
+        </div>
+        <div className={styles.description}>
+          <GameCover image={game.image} />
+          <p>{game.description}</p>
+          <p className={[styles["secondary-text"]]}>
+            Популярные метки этого продукта:
+          </p>
+          {game.genres.map((genre) => (
+            <GameGenre genre={genre} key={genre} />
+          ))}
+          <div className={[styles["buy-game"]]}>
+            <GameBuy game={game} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
