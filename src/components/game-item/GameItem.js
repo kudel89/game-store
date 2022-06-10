5c86dd37855b3b7e3c11acc0c880ecec8134bd95
Eugene Kudelya <kudel89@gmail.com>
2022-06-10 11:19:03 +0300

    Added navigation to the GamePage by clicking on GameItem card

--- src/components/game-item/GameItem.js
+++ src/components/game-item/GameItem.js
@@ -1,12 +1,22 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router";
+import { setCurrentGame } from "../../redux/games/reducer";
 import { GameCover } from "../game-cover/GameCover";
 import { GameBuy } from "../game-buy/GameBuy";
 import { GameGenre } from "./../game-genre/GameGenre";
 import styles from "./GameItem.module.css";
 
 export const GameItem = ({ game }) => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const handleClick = () => {
+    dispatch(setCurrentGame(game));
+    navigate(`/app/${game.title}`);
+  };
+
   return (
-    <div className={styles.card}>
+    <div className={styles.card} onClick={handleClick}>
       <div className={styles.container}>
         <GameCover image={game.image} />
 
