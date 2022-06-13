17f6c271ca8db8ba4b5f01e86826e600d46f8c4c
Eugene Kudelya <kudel89@gmail.com>
2022-06-13 12:59:33 +0300

    Moved the BrowserRouter to the App component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./redux";
 import { HomePage } from "./pages/home-page/HomePage";
 import { GamePage } from "./pages/game-page/GamePage";
+import { OrderPage } from "./pages/order-page/OrderPage";
 import { Header } from "./components/header/Header";
 
 function App() {
   return (
     <Provider store={store}>
-      <div className="App">
-        <Header />
-        <div className="wrapper">
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/app/:title" element={<GamePage />} />
-          </Routes>
+      <Router>
+        <div className="App">
+          <Header />
+          <div className="wrapper">
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/app/:title" element={<GamePage />} />
+              <Route path="/order" element={<OrderPage />} />
+            </Routes>
+          </div>
         </div>
-      </div>
+      </Router>
     </Provider>
   );
 }
