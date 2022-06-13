1b72b1f0f92f3261b8610222e263bb9aa218b82f
Eugene Kudelya <kudel89@gmail.com>
2022-06-13 13:17:12 +0300

    Added logic to the OrderITem

--- src/components/order-item/OrderItem.js
+++ src/components/order-item/OrderItem.js
@@ -1,15 +1,32 @@
 import React from "react";
+import { useDispatch } from "react-redux";
 import { GameCover } from "./../game-cover/GameCover";
+import { deleteItemFromCart } from "../../redux/cart/reducer";
+import { AiOutlineCloseCircle } from "react-icons/ai";
 import styles from "./OrderItem.module.css";
 
 export const OrderItem = ({ game }) => {
+  const dispatch = useDispatch();
+  const handleClick = () => {
+    dispatch(deleteItemFromCart(game.id));
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.cover}>
         <GameCover image={game.image} />
       </div>
-      <div className={styles.title}>{game.title}</div>
-      <div className={styles.price}>{game.price} грн.</div>
+      <div className={styles.title}>
+        <span>{game.title}</span>
+      </div>
+      <div className={styles.price}>
+        <span>{game.price} грн.</span>
+        <AiOutlineCloseCircle
+          size={25}
+          className={styles.delete}
+          onClick={handleClick}
+        />
+      </div>
     </div>
   );
 };
